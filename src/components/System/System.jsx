8e967be93472769a0af78085e178e6963f3ea124
ds1242 <david.shaw1242@gmail.com>
2021-12-28 20:19:55 -0700

    system images imported and inital design set

--- src/components/System/System.jsx
+++ src/components/System/System.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-
+import { Container, Row, Col } from 'react-bootstrap';
+import pump from '../../assets/pump.png';
+import sun from '../../assets/sun.png';
+import solarPanel from '../../assets/solar_panel.png';
+import storageTank from '../../assets/storage_tank.png';
 
 function System() {
     return (
-        <h1>System Body</h1>
+        <Container>
+            <Row>
+                <Col>
+                    <img src={sun} />
+                </Col>
+                <Col>
+                </Col>
+                <Col>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <img src={solarPanel} />
+                </Col>
+                <Col>
+                    <img src={pump} />
+                </Col>
+                <Col>
+                    <img src={storageTank} />
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
